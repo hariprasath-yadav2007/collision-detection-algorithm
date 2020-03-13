9b93dcbdbe92ec5c4b83aef958bfa2917db1d365
hariprasath-yadav2007 <62061411+hariprasath-yadav2007@users.noreply.github.com>
2020-03-13 04:31:12 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# collision-detection-algorithm
-the objects change color when they collide
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var movingrect, fixrect;
+
+function setup() {
+  createCanvas(800,400);
+  movingrect = createSprite(400, 200, 100, 50);
+  movingrect.shapeColor = "red";
+  fixrect  = createSprite(400,200,50,50);
+  fixrect.shapeColor = "red";
+  movingrect.debug = true;
+  fixrect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingrect.x = mouseX;
+  movingrect.y = mouseY;
+  if(movingrect.x - fixrect.x < movingrect.width/2 + fixrect.width/2
+    && fixrect.x - movingrect.x < movingrect.width/2 + fixrect.width/2
+    && movingrect.y - fixrect.y < movingrect.height/2 + fixrect.height/2
+    && fixrect.y - movingrect.y < movingrect.height/2 + fixrect.height/2){
+    movingrect.shapeColor = "yellow";
+    fixrect.shapeColor = "yellow";
+  }
+  else{
+    movingrect.shapeColor = "red";
+    fixrect.shapeColor = "red";
+  }
+  drawSprites();
+ 
+}
